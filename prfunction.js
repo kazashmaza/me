23bb90a09a1347e6ff49c8428b925f61279921e6
kazashmaza <123529532+kazashmaza@users.noreply.github.com>
2023-01-25 16:17:59 +1100

    Update prfunction.js

--- prfunction.js
+++ prfunction.js
@@ -13,21 +13,32 @@
     var recordLabel = document.getElementById("record-label").value;
     var genre = document.getElementById("genre").value;
     var trackList = document.getElementById("track-list").value.split(', ');
+    var artistBio = document.getElementById("artist-bio").value;
+var inspiration = document.getElementById("inspiration").value;
+var artistQuote = document.getElementById("artist-quote").value;
+
+    
+    
 
     // press release template
-    var pressRelease = `
-    Press Release
+   var pressRelease = `
+Press Release
 
-    ${artistName} Announces the Release of New Album ${albumName}
+${artistName} Announces the Release of New Album ${albumName}
 
-    ${artistName}, an up-and-coming artist in the ${genre} scene, is excited to announce the release of their new album ${albumName} on ${releaseDate.toDateString()}. The album, which is
-being released under ${recordLabel}, features the following tracks:
+${artistName}, an up-and-coming artist in the ${genre} scene, is excited to announce the release of their new album ${albumName} on ${releaseDate.toDateString()}. The album, which is being released under ${recordLabel}, features the following tracks:
+
 ${trackList.map((track, index) => `${index + 1}. ${track}`).join('\n')}
 
-This new album represents a significant step forward in ${artistName}'s career and is sure to be a hit with fans of ${genre} music.
+${artistName} says, "${artistQuote}"
 
-"I'm thrilled to finally be releasing ${albumName}. I've put so much into this album and I can't wait for everyone to hear it," said ${artistName}
+${artistName} was inspired by ${inspiration} when creating ${albumName}.
+
+${artistBio}
+
+This new album represents a significant step forward in ${artistName}'s career and is sure to be a hit with fans of ${genre} music.
 `;
+
 
 // Get the div element where the press release will be displayed
 var pressReleaseDiv = document.getElementById("generated-press-release");
